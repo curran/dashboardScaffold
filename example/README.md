4b38a97588db3f30e31e4a011cd77b85b1cf6dd9
Curran <curran.kelleher@gmail.com>
2013-07-24 14:50:41 -0400

    Refactored directories - separated the example from the lib

---
+++ example/README.md
@@ -0,0 +1,1 @@
+An example project that uses the dashboardScaffold library.
---
+++ example/js/main.js
@@ -0,0 +1,3 @@
+define(['dashboardScaffold'], function(dashboardScaffold){
+  dashboardScaffold.init('editor', 'dashboard');
+});
