839a61e021310c81711644fd45f8e08476df0294
Curran <curran.kelleher@gmail.com>
2013-07-30 09:00:45 -0400

    Added print statement to static file server that shows URL being served

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // Run with the shell command "node server.js".
 // (first install Express with the command "npm install express")
 // (first install Node.js, see https://github.com/joyent/node/wiki/Installation
-var express = require('express'),
+var port = 8000,
+    express = require('express'),
     app = express();
 app.use('/', express.static(__dirname));
-app.listen('8000');
+app.listen(port);
+console.log('Now serving http://localhost:'+port);
