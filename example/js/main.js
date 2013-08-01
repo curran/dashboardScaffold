706c1c5577acf4eab6674be0b09f4d4e11efed70
Curran <curran.kelleher@gmail.com>
2013-08-01 15:43:00 -0400

    Added example page that just loads the dashboard without the configuration editor.

--- example/js/main.js
+++ example/js/main.js
@@ -1,3 +1,18 @@
-define(['dashboardScaffold'], function(dashboardScaffold){
-  dashboardScaffold.init('editor', 'dashboard');
+define(['dashboardScaffold', 'dashboardScaffold/dashboard'],
+    function(dashboardScaffold, Dashboard){
+  var editor = document.getElementById('editor');
+  if(editor){
+    dashboardScaffold.init('editor', 'dashboard');
+  }
+  else{
+    d3.json('dashboardConfig.json', function(config){
+      var dashboard = Dashboard.createDashboard('dashboard');
+      console.log(config);
+      dashboard.setConfig(config);
+
+      window.addEventListener('resize', _.debounce(function(){
+        dashboard.update();
+      }, 100));
+    });
+  }
 });
