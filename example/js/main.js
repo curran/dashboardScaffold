1ba4268e9e3705e878a356de28bcf69ba6e6c6b5
Curran <curran.kelleher@gmail.com>
2013-08-05 13:29:57 -0400

    Got example/js/main.js to pass JSLint

--- example/js/main.js
+++ example/js/main.js
@@ -1,18 +1,19 @@
-define(['dashboardScaffold', 'dashboardScaffold/dashboard'],
-    function(dashboardScaffold, Dashboard){
-  var editor = document.getElementById('editor');
-  if(editor){
-    dashboardScaffold.init('editor', 'dashboard');
-  }
-  else{
-    d3.json('dashboardConfig.json', function(config){
-      var dashboard = Dashboard.createDashboard('dashboard');
-      console.log(config);
-      dashboard.setConfig(config);
+/*global define, document, window */
+/*jslint nomen: true */ // this causes JSLint to tolerate "_"
+define(['d3', 'underscore', 'dashboardScaffold', 'dashboardScaffold/dashboard'], function (d3, _, dashboardScaffold, Dashboard) {
+    "use strict";
+    var editor = document.getElementById('editor');
+    if (editor) {
+        dashboardScaffold.init('editor', 'dashboard');
+    } else {
+        d3.json('dashboardConfig.json', function (config) {
+            var dashboard = Dashboard.createDashboard('dashboard');
+            console.log(config);
+            dashboard.setConfig(config);
 
-      window.addEventListener('resize', _.debounce(function(){
-        dashboard.update();
-      }, 100));
-    });
-  }
+            window.addEventListener('resize', _.debounce(function () {
+                dashboard.update();
+            }, 100));
+        });
+    }
 });
