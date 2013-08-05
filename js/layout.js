7d2f60d3dd915ec19cf393443ed6f3cb09785bc0
Curran <curran.kelleher@gmail.com>
2013-08-05 11:28:17 -0400

    Got layout.js to pass JSLint

--- js/layout.js
+++ js/layout.js
@@ -1,70 +1,69 @@
+/*global define */
+/*jslint nomen: true */ // this causes JSLint to tolerate "_"
+
 /**
  * A simple layout algorithm for nested boxes that uses D3 conventions.
  */
-define(['underscore'], function(_){
-  return function(root, visOptions){
-    var leaves = [];
-    function hidden(node){
-      var options = visOptions[node.name];
-      if(options){ return options.hidden; }
-      else{ return childrenAreHidden(node); }
-    }
-    function childrenAreHidden(node){
-      if(node.children){
-        return _.every(node.children, function(child){
-          var options = visOptions[child.name];
-          if(options){ return options.hidden; }
-          else { return false; }
-        });
-      }
-      else{
-        return false;
-      }
-    }
-    function innerLayout(node, x, y, dx, dy){
-      var totalSize = 0, i, child,
-          childX, childY, childDx, childDy;
-      for(i = 0; i < node.children.length; i++){
-        child = node.children[i];
-        if(!hidden(child)){
-          totalSize += child.size;
+define(['underscore'], function (_) {
+    "use strict";
+    return function (root, visOptions) {
+        var leaves = [];
+        function childrenAreHidden(node) {
+            if (node.children) {
+                return _.every(node.children, function (child) {
+                    var options = visOptions[child.name];
+                    if (options) { return options.hidden; }
+                    return false;
+                });
+            }
+            return false;
         }
-      }
-      for(i = 0; i < node.children.length; i++){
-        child = node.children[i];
-        if(!hidden(child)){
-          if(node.orientation == 'horizontal'){
-            childDx = dx * child.size / totalSize;
-            childX = x;
-            childY = y;
-            childDx = childDx;
-            childDy = dy;
-            x += childDx;
-          }
-          else if(node.orientation == 'vertical'){
-            childDy = dy * child.size / totalSize;
-            childX = x;
-            childY = y;
-            childDx = dx;
-            childDy = childDy;
-            y += childDy;
-          }
+        function hidden(node) {
+            var options = visOptions[node.name];
+            if (options) {
+                return options.hidden;
+            }
+            return childrenAreHidden(node);
         }
-        if(child.children){
-          innerLayout(child, childX, childY, childDx, childDy);
+        function innerLayout(node, x, y, dx, dy) {
+            var totalSize = 0, i,
+                childX, childY, childDx, childDy;
+            _.each(node.children, function (child) {
+                if (!hidden(child)) {
+                    totalSize += child.size;
+                }
+            });
+            _.each(node.children, function (child) {
+                if (!hidden(child)) {
+                    if (node.orientation === 'horizontal') {
+                        childDx = dx * child.size / totalSize;
+                        childX = x;
+                        childY = y;
+                        childDy = dy;
+                        x += childDx;
+                    } else if (node.orientation === 'vertical') {
+                        childDy = dy * child.size / totalSize;
+                        childX = x;
+                        childY = y;
+                        childDx = dx;
+                        y += childDy;
+                    }
+                }
+
+                if (child.children) {
+                    innerLayout(child, childX, childY, childDx, childDy);
+                } else {
+                    leaves.push({
+                        d: child,
+                        x: childX,
+                        y: childY,
+                        dx: childDx,
+                        dy: childDy
+                    });
+                }
+            });
         }
-        else{
-          leaves.push({
-            d: child,
-            x: childX,
-            y: childY,
-            dx: childDx,
-            dy: childDy
-          });
-        }
-      }
-    }
-    innerLayout(root, 0, 0, 100, 100);
-    return leaves;
-  };
+        innerLayout(root, 0, 0, 100, 100);
+        return leaves;
+    };
 });
