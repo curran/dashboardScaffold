c01cc7fcab1e20594228354d8db11768c169418b
Curran <curran.kelleher@gmail.com>
2013-08-05 13:44:23 -0400

    Got example/js/vis.js to pass JSLint

--- example/js/vis.js
+++ example/js/vis.js
@@ -1,124 +1,120 @@
+/*global define, document */
+/*jslint nomen: true */ // this causes JSLint to tolerate "_"
+
 /**
  * A dummy visualization to show how D3 visualizations
  * can work within the dashboard layout framework.
  */
-define(['d3', 'underscore', 'getterSetters'], function(d3, _, getterSetters){
-  return function(){
-    var div = document.createElement('div'),
-        options = {
-          bkgColor: '#005E47',
-          lineColor: '#000000',
-          lineWidth: 5,
-          labelSize: '20px',
-          labelText: '',
-          width: 0,
-          height: 0
+define(['d3', 'underscore', 'getterSetters'], function (d3, _, getterSetters) {
+    "use strict";
+    return function () {
+        var div = document.createElement('div'),
+            my,
+            options = {
+                bkgColor: '#005E47',
+                lineColor: '#000000',
+                lineWidth: 5,
+                labelSize: '20px',
+                labelText: '',
+                width: 0,
+                height: 0
+            },
+            svg = function () {
+                var theSVG = d3.select(div).selectAll('svg').data([1]);
+                theSVG.enter().append('svg');
+                theSVG
+                    .attr('width', options.width)
+                    .attr('height', options.height);
+                return theSVG;
+            },
+            randomColor = function () {
+                var r = Math.round(Math.random() * 255),
+                    g = Math.round(Math.random() * 255),
+                    b = Math.round(Math.random() * 255);
+                return d3.rgb(r, g, b).toString();
+            },
+            updateRect = _.debounce(function () {
+                console.log('in updateRect');
+                var rect = svg().selectAll('rect').data([1]);
+                rect.enter().append('rect')
+                    .on('click', function () {
+                        my.bkgColor(randomColor());
+                    });
+                rect.attr('x', 0)
+                    .attr('y', 0)
+                    .attr('width', options.width)
+                    .attr('height', options.height)
+                    .attr('fill', options.bkgColor);
+            }, 0),
+            updateLines = _.debounce(function () {
+                console.log('in updateLines');
+                var w = options.width,
+                    h = options.height,
+                    lines = svg().selectAll('line').data([
+                        {x1: 0, y1: 0, x2: w, y2: h},
+                        {x1: 0, y1: h, x2: w, y2: 0}
+                    ]),
+                    x1,
+                    width1,
+                    drag = d3.behavior.drag()
+                        .on('dragstart', function (d) {
+                            x1 = d3.event.sourceEvent.pageX;
+                        })
+                        .on('drag', function (d) {
+                            var x2 = d3.event.sourceEvent.pageX,
+                                newLineWidth = my.lineWidth() + x2 - x1;
+                            newLineWidth = newLineWidth < 1 ? 1 : newLineWidth;
+                            my.lineWidth(newLineWidth);
+                            x1 = x2;
+                        });
+
+                lines.enter().append('line')
+                    .on('click', function () {
+                        my.lineColor(randomColor());
+                    });
+
+                lines
+                    .attr('x1', function (d) { return d.x1; })
+                    .attr('y1', function (d) { return d.y1; })
+                    .attr('x2', function (d) { return d.x2; })
+                    .attr('y2', function (d) { return d.y2; })
+                    .style('stroke', options.lineColor)
+                    .style('stroke-width', options.lineWidth)
+                    .call(drag);
+            }, 0),
+            updateLabel = _.debounce(function () {
+                var label = svg().selectAll('text').data([1]);
+                label.enter().append('text');
+                label
+                    .attr('x', options.width / 2)
+                    .attr('y', options.height / 2)
+                    .attr('text-anchor', 'middle')
+                    .attr('dy', '0.5em')
+                    .style('font-size', options.labelSize)
+                    .text(options.labelText);
+                console.log('in updateLabel');
+            }, 0);
+
+        // See http://bost.ocks.org/mike/chart/
+        my = function () {
+            console.log('in my');
+            updateRect();
+            updateLines();
+            updateLabel();
         };
+        _.extend(my, getterSetters(options));
 
-    function svg(){
-      var svg = d3.select(div).selectAll('svg').data([1]);
-      svg.enter().append('svg');
-      svg.attr('width', options.width)
-         .attr('height', options.height);
-      return svg;
-    }
+        my.width.on('change', my);
+        my.height.on('change', my);
+        my.bkgColor.on('change', updateRect);
+        my.lineColor.on('change', updateLines);
+        my.lineWidth.on('change', updateLines);
+        my.labelText.on('change', updateLabel);
+        my.labelSize.on('change', updateLabel);
 
-    function randomColor(){
-      var r = Math.round(Math.random() * 255),
-          g = Math.round(Math.random() * 255),
-          b = Math.round(Math.random() * 255);
-      return d3.rgb(r, g, b).toString();
-    }
-
-    var updateRect = _.debounce(function(){
-      console.log('in updateRect');
-      var rect = svg().selectAll('rect').data([1]);
-      rect.enter().append('rect')
-        .on('click', function(){
-          my.bkgColor(randomColor());
-        });
-      rect
-        .attr('x', 0)
-        .attr('y', 0) 
-        .attr('width', options.width)
-        .attr('height', options.height)
-        .attr('fill', options.bkgColor);
-    }, 0);
-
-    var updateLines = _.debounce(function(){
-      console.log('in updateLines');
-      var w = options.width,
-          h = options.height,
-          lines = svg().selectAll('line').data([
-            {x1: 0, y1: 0, x2: w, y2: h},
-            {x1: 0, y1: h, x2: w, y2: 0}
-          ]),
-          x1,
-          width1,
-          drag = d3.behavior.drag()
-            .on('dragstart', function(d){
-              x1 = d3.event.sourceEvent.pageX;
-            })
-            .on('drag', function(d){
-              var x2 = d3.event.sourceEvent.pageX,
-                  newLineWidth = my.lineWidth() + x2 - x1;
-              newLineWidth = newLineWidth < 1 ? 1 : newLineWidth;
-              my.lineWidth(newLineWidth);
-              x1 = x2
-            });
-
-      lines.enter().append('line')
-        .on('click', function(){
-          my.lineColor(randomColor());
-        });
-
-
-
-
-      lines
-        .attr('x1', function(d){ return d.x1; })
-        .attr('y1', function(d){ return d.y1; })
-        .attr('x2', function(d){ return d.x2; })
-        .attr('y2', function(d){ return d.y2; })
-        .style('stroke', options.lineColor)
-        .style('stroke-width', options.lineWidth)
-        .call(drag);
-    }, 0);
-
-    var updateLabel = _.debounce(function(){
-      console.log('in updateLabel');
-      var label = svg().selectAll('text').data([1]);
-      label.enter().append('text');
-      label
-        .attr('x', options.width / 2)
-        .attr('y', options.height / 2)
-        .attr('text-anchor', 'middle')
-        .attr('dy', '0.5em')
-        .style('font-size', options.labelSize)
-        .text(options.labelText);
-    }, 0);
-
-    // See http://bost.ocks.org/mike/chart/
-    function my(){
-      console.log('in my');
-      updateRect();
-      updateLines();
-      updateLabel();
-    }
-    
-    _.extend(my, getterSetters(options));
-
-    my.width.on('change', my);
-    my.height.on('change', my);
-    my.bkgColor.on('change', updateRect);
-    my.lineColor.on('change', updateLines);
-    my.lineWidth.on('change', updateLines);
-    my.labelText.on('change', updateLabel);
-    my.labelSize.on('change', updateLabel);
-
-    return {
-      domElement: div,
-      chart: my
-    }
-  }
+        return {
+            domElement: div,
+            chart: my
+        };
+    };
 });
