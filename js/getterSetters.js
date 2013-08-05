bf7a990191c1b82aa495313d7121e2d3edcc1e90
Curran <curran.kelleher@gmail.com>
2013-08-05 11:19:08 -0400

    Made getterSetters comply with JSLint

--- js/getterSetters.js
+++ js/getterSetters.js
@@ -1,3 +1,6 @@
+/*global define */
+/*jslint nomen: true */ // this causes JSLint to tolerate "_"
+
 /**
  * A generator for getter-setter-with-change-event functions.
  * Takes as input an options object, returns an object whose
@@ -8,25 +11,24 @@
  * of the setter-getter function pattern.
  *
  * In addition to this pattern, the functions emit change events.
- * To listen, use `theFunction.on('change', function(newValue){ ... });`
+ * To listen, use `theFunction.on('change', function (newValue) { ... });`
  */
-define(['underscore', 'backbone'], function(_, Backbone){
-  return function(options){
-    var my = {};
-    _.each(_.keys(options), function(property){
-      var fn = function(value){
-        if (!arguments.length){
-          return options[property];
-        }
-        else{
-          options[property] = value;
-          fn.trigger('change', value);
-        }
+define(['underscore', 'backbone'], function (_, Backbone) {
+    "use strict";
+    return function (options) {
+        var my = {};
+        _.each(_.keys(options), function (property) {
+            var fn = function (value) {
+                if (!arguments.length) {
+                    return options[property];
+                }
+                options[property] = value;
+                fn.trigger('change', value);
+                return my;
+            };
+            _.extend(fn, Backbone.Events);
+            my[property] = fn;
+        });
         return my;
-      };
-      _.extend(fn, Backbone.Events);
-      my[property] = fn;
-    });
-    return my;
-  };
+    };
 });
