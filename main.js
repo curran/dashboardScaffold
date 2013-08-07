74fa33517f1aed317dc084c766c328b1aadd95a4
Curran <curran.kelleher@gmail.com>
2013-08-07 11:53:49 -0400

    Refactored initialization logic

--- main.js
+++ main.js
@@ -4,71 +4,66 @@ define(['codeMirror', 'inlet', 'd3', 'underscore', './dashboard'], function (Cod
 
     "use strict";
 
+    function init (config, dashboardId, editorId) {
+        var dashboard = Dashboard.createDashboard(dashboardId),
+            editor = document.getElementById(editorId);
+        if (editor) { initEditor(config, dashboard, editor); }
+        dashboard.setConfig(config);
+        return dashboard;
+    }
+
+    function initEditor(config, dashboard, editor){
+        var codeMirror = CodeMirror.fromTextArea(editor),
+            settingConfig = false,
+            invalidJSONConfig = {
+                "layout": {
+                    "orientation": "vertical",
+                    "children": [ { "name": "vis", "size": 1 } ]
+                },
+                "visualizations": {
+                    "vis": {
+                        "module": "vis",
+                        "bkgColor": "red",
+                        "lineColor": "red",
+                        "labelText": "Invalid JSON",
+                        "labelSize": "41pt"
+                    }
+                },
+                "visDivCSS": {
+                    "border-style": "solid",
+                    "border-width": "2px"
+                }
+            };
+        inlet(codeMirror);
+        codeMirror.setOption('mode', 'javascript');
+        codeMirror.setSize('100%', '100%');
+        codeMirror.setOption('value', JSON.stringify(config, null, 2));
+        codeMirror.on('change', function () {
+            settingConfig = true;
+            try {
+                // This line will throw if parsing fails
+                dashboard.setConfig(JSON.parse(codeMirror.getValue()));
+            } catch (e) {
+                dashboard.setConfig(invalidJSONConfig);
+            }
+            settingConfig = false;
+        });
+        dashboard.on('configChanged', function (config) {
+            if (!settingConfig) {
+                codeMirror.setOption('value', JSON.stringify(config, null, 2));
+            }
+        });
+    }
+
     return {
         /**
          * Initializes the dashboard configurator system.
          * Arguments:
          *
+         *  * `config` The object parsed from the dashboard configuration JSON.
          *  * `editorId` The id of the textArea DOM element that should become the configuration editor.
          *  * `dashboardId` The id of the div DOM element that the dashboard will be injected into.
          */
-        init: function (editorId, dashboardId) {
-            var editor = document.getElementById(editorId),
-                codeMirror = CodeMirror.fromTextArea(editor),
-                dashboard = Dashboard.createDashboard(dashboardId),
-                invalidJSONConfig = {
-                    "layout": {
-                        "orientation": "vertical",
-                        "children": [ { "name": "vis", "size": 1 } ]
-                    },
-                    "visualizations": {
-                        "vis": {
-                            "module": "vis",
-                            "bkgColor": "red",
-                            "lineColor": "red",
-                            "labelText": "Invalid JSON",
-                            "labelSize": "41pt"
-                        }
-                    },
-                    "visDivCSS": {
-                        "border-style": "solid",
-                        "border-width": "2px"
-                    }
-                };
-
-            window.addEventListener('resize', _.debounce(function () {
-                dashboard.update();
-            }, 100));
-
-
-            inlet(codeMirror);
-            codeMirror.setOption('mode', 'javascript');
-            codeMirror.setSize('100%', '100%');
-            d3.text('dashboardConfig.json', function (configJSON) {
-                // A flag to prevent redundantly resetting the
-                // text every time the use changes it.
-                var settingConfig = false;
-
-                codeMirror.setOption('value', configJSON);
-                dashboard.setConfig(JSON.parse(configJSON));
-
-                codeMirror.on('change', function () {
-                    settingConfig = true;
-                    try {
-                        // This line will throw if parsing fails
-                        dashboard.setConfig(JSON.parse(codeMirror.getValue()));
-                    } catch (e) {
-                        dashboard.setConfig(invalidJSONConfig);
-                    }
-                    settingConfig = false;
-                });
-                dashboard.on('configChanged', function (config) {
-                    if (!settingConfig) {
-                        codeMirror.setOption('value', JSON.stringify(config, null, 2));
-                    }
-                });
-            });
-            return dashboard;
-        }
+        init: init
     };
 });
